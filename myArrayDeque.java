041d8098b23895bc792379580d9f39ded5b30706
YashGoyal-web <62822789+YashGoyal-web@users.noreply.github.com>
2020-08-24 13:18:07 +0530

    Update myArrayDeque.java

--- myArrayDeque.java
+++ myArrayDeque.java
@@ -9,18 +9,11 @@ public class MainClassArrayDeque {
         myDoublyLinkedList<Integer> mDLL = new myDoublyLinkedList();
         
         Queue q = new LinkedList();
-        
         mDLL.insertFirst(1);
         mDLL.insertFirst(2);
         mDLL.insertFirst(3);
         mDLL.insertLast(4);
                 
-        mDLL.removeFirst();
-        mDLL.removeLast();
-        
-        mDLL.removeFirst();
-        mDLL.removeLast();
-
         mDLL.show();
         
         mDLL.insertLast(4);
